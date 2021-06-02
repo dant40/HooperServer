38e6da110133ca4f9ef6c68688b74f9db06ddece
dant40 <32370199+dant40@users.noreply.github.com>
2021-06-01 21:34:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,24 +3,28 @@ $().ready(function() {
    //remember: decimal point => %2E
    var lat = parseFloat(urlParams.get("lat"));
    var long = parseFloat(urlParams.get("long"));
-   console.log("test3");
+   console.log("test4");
    
    $.getJSON( "courts.json", function( data ) {   
       var resObject = new Object();
       var distances = [];
       var dist;
-      console.log(data["courts"])
       var courts = data["courts"];
       for(var i = 0; i < courts.length;i++){
          let item = courts[i];
-         console.log(item["latitude"]);
             let x = item["latitude"] - lat;
             x *= x;
             let y = item["longitude"] - long;
             y *= y;
             dist = Math.sqrt(x + y);
-            distances.push(dist);
+            var tempObj = new Object();
+            tempObj.distance = dist;
+            tempObj.index = i;
+            distances.push(tempObj);
       }
+      distances.sort(function (a, b) {
+         return a.distance - b.distance;);
+         
       console.log(distances);
       //return resOjbect;
   });
