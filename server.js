424255ca33f0f0184162491c66d88beab2b25305
dant40 <32370199+dant40@users.noreply.github.com>
2021-06-01 20:17:21 -0400

    parse url params and calculate distances

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 $().ready(function() {
    var urlParams = new URLSearchParams(window.location.search);
-   console.log(urlParams.toString());
-   $.getJSON( "courts.json", function( data ) {   
-      console.log(data);
+   //remember: decimal point => %2E
+   var lat = parseFloat(urlParams.get("lat"));
+   var long = parseFloat(urlParams.get("long"));
+   console.log(lat);
+   console.log(long);
    
+   $.getJSON( "courts.json", function( data ) {   
+      var resObject = new Object();
+      var distances = [];
+      var dist;
+      $.each(data, function(index, item){
+            let x = item["latitude"] - lat;
+            x *= x;
+            let y = item["longitude"] - long;
+            y *= y;
+            dist = Math.sqrt(x + y);
+            distances.push(dist);
+      });
+      console.log(distances);
+      //return resOjbect;
   });
 });
